80f574be0b11f19c176036984645c719e829fa1f
Matheesh <matheeshprabhu123@gmail.com>
2021-11-25 17:57:58 +0530

    ADding test methods for BeerClient

--- src/test/java/com/math/microbeerclient/web/client/BeerClientTest.java
+++ src/test/java/com/math/microbeerclient/web/client/BeerClientTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.net.URI;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,9 +17,31 @@ class BeerClientTest {
     BeerClient beerClient;
 
     @Test
-    void getBeerById() {
+    void getBeerByIdTest() {
         BeerDto beerDto = beerClient.getBeerById(UUID.randomUUID());
         assertNotNull(beerDto);
-        System.out.println(beerDto.getBeerName() + " " + beerDto.getId());
+        System.out.println(beerDto.getBeerName() + " " + beerDto.getBeerStyle() + " " + beerDto.getId());
+    }
+
+    @Test
+    public void saveNewBeerTest(){
+
+        BeerDto beerDto = BeerDto.builder().beerName("test beer").build();
+        URI uri = beerClient.saveNewBeer(beerDto);
+
+        assertNotNull(uri);
+        System.out.println(uri);
+    }
+
+    @Test
+    public void updateBeerTest(){
+
+        BeerDto beerDto = BeerDto.builder().beerStyle("can").build();
+        beerClient.updateBeer(UUID.randomUUID(),beerDto);
+    }
+
+    @Test
+    public void deleteBeer(){
+        beerClient.deleteBeer(UUID.randomUUID());
     }
 }
