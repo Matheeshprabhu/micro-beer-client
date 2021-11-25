f0eab83fc3bf13d753e60996b3678310881b8794
Matheesh <matheeshprabhu123@gmail.com>
2021-11-25 17:56:43 +0530

    Adding save and update beer methods

--- src/main/java/com/math/microbeerclient/web/client/BeerClient.java
+++ src/main/java/com/math/microbeerclient/web/client/BeerClient.java
@@ -6,10 +6,11 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.UUID;
 
 @Component
-@ConfigurationProperties(value = "math.beer", ignoreUnknownFields = false)
+@ConfigurationProperties(prefix = "math.beer", ignoreUnknownFields = false)
 public class BeerClient {
 
     public final String BEER_PATH_V1 = "/api/v1/beer/";
@@ -26,5 +27,17 @@ public class BeerClient {
 
     public BeerDto getBeerById(UUID id){
         return restTemplate.getForObject(apihost + BEER_PATH_V1 + id, BeerDto.class);
+    }
+
+    public URI saveNewBeer(BeerDto beerDto){
+        return restTemplate.postForLocation(apihost + BEER_PATH_V1, beerDto);
+    }
+
+    public void updateBeer(UUID id, BeerDto beerDto){
+        restTemplate.put(apihost + BEER_PATH_V1 + id, beerDto);
+    }
+
+    public void deleteBeer(UUID id){
+        restTemplate.delete(apihost + BEER_PATH_V1 + id);
     }
 }
