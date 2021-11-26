0159ad1633fafb990b013ce900d66e47745e4035
Matheesh <matheeshprabhu123@gmail.com>
2021-11-26 22:11:16 +0530

    Adding custom rest template - apache client

---
+++ src/main/java/com/math/microbeerclient/web/config/BlockingRestTemplateCustomizer.java
@@ -0,0 +1,54 @@
+package com.math.microbeerclient.web.config;
+
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateCustomizer;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+@PropertySource("classpath:application-client.properties")
+public class BlockingRestTemplateCustomizer implements RestTemplateCustomizer {
+
+    @Value("${math.client.max_total}")
+    private int maxTotal;
+    @Value("${math.client.max_per_route}")
+    private int maxPerRoute;
+    @Value("${math.client.connection_request_timeout}")
+    private int requestTimeout;
+    @Value("${math.client.socket_timeout}")
+    private int socketTimeout;
+
+    public ClientHttpRequestFactory clientHttpRequestFactory(){
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setMaxTotal(maxTotal);
+        connectionManager.setDefaultMaxPerRoute(maxPerRoute);
+
+        RequestConfig requestConfig = RequestConfig
+                .custom()
+                .setConnectionRequestTimeout(requestTimeout)
+                .setSocketTimeout(socketTimeout)
+                .build();
+
+        CloseableHttpClient httpClient = HttpClients
+                .custom()
+                .setConnectionManager(connectionManager)
+                .setKeepAliveStrategy(new DefaultConnectionKeepAliveStrategy())
+                .setDefaultRequestConfig(requestConfig)
+                .build();
+
+        return new HttpComponentsClientHttpRequestFactory(httpClient);
+    }
+
+    @Override
+    public void customize(RestTemplate restTemplate) {
+        restTemplate.setRequestFactory(this.clientHttpRequestFactory());
+    }
+}
